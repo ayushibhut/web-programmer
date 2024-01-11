b2c57036357fcc4d2d4a01df7b21b66dd1abcb4a
ayushibhut <155126405+ayushibhut@users.noreply.github.com>
2024-01-11 08:44:35 +0530

    Add files via upload

---
+++ string.php
@@ -0,0 +1,23 @@
+<?php
+
+//string function
+
+echo strlen("Hell, world!");
+echo "<br>";
+echo strpos("Hello, World!","World");
+echo "<br>";
+echo str_replace("World","Universe","Hello, World!");
+echo "<br>";
+echo substr("Hello,World!",0,5);
+echo "<br>";
+echo strtolower("Hello, World!");
+echo "<br>";
+echo strtoupper("Hello, World!");
+echo "<br>";
+echo ucfirst("Hello, World!");
+echo "<br>";
+echo strrev("Hello, World!");
+echo "<br>";
+echo trim("Hello, World!");
+
+?>
