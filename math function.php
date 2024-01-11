c1c533b552c16be4ca3047f847fb2b439a09feb6
ayushibhut <155126405+ayushibhut@users.noreply.github.com>
2024-01-11 08:34:24 +0530

    Add files via upload

---
+++ math function.php
@@ -0,0 +1,25 @@
+<?php
+
+//math function
+
+echo abs(-5);
+echo "<br>";
+echo sqrt(16);
+echo "<br>";
+echo rand(1,100);
+echo "<br>";
+echo round(3.7);
+echo "<br>";
+echo ceil(3.2);
+echo "<br>";
+echo floor(3.8);
+echo "<br>";
+echo pow(2,3);
+echo "<br>";
+echo max(5,8,3);
+echo "<br>";
+echo min(5,8,3);
+
+?>
+
+
