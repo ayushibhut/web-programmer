c1ce2b935b55e0b2e21b127f633b5460319f8eb7
ayushibhut <155126405+ayushibhut@users.noreply.github.com>
2024-02-26 08:21:28 +0530

    Add files via upload

---
+++ array/array.php
@@ -0,0 +1,27 @@
+<?php
+$student=array("Ayushi","BCA",8,220801041,"Atmiya","Computer science","php","java","english","DS");
+echo $student[0];
+echo "<br>";
+echo $student[1];
+echo "<br>";
+echo $student[2];
+echo "<br>";
+echo $student[3];
+echo "<br>";
+echo $student[4];
+echo "<br>";
+echo $student[5];
+echo "<br>";
+echo $student[6];
+echo "<br>";
+echo $student[7];
+echo "<br>";
+echo $student[8];
+echo "<br>";
+echo $student[9];
+
+
+
+
+
+?>
